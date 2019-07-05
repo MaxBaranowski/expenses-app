1c44d734cef3d2fc433e7da9b624884e817f822b
Max Baranowski <maksim.baranowski@unilinkcash.pl>
2019-07-05 16:44:06 +0200

    autoryzacja poprawka zapisaywania sessji

---
+++ app/src/components/UserExpensesTable/components/Budget.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="UserExpensesTableBudget">budget table wiil be here</div>
+</template>
+
+<script>
+const LOGIN_URL = "http://localhost:3000/user/logout";
+
+import "../UserExpensesTable.scss";
+
+export default {
+  name: "UserExpensesTableBudget",
+  data() {
+    return {};
+  },
+  methods: {},
+  mounted() {}
+};
+</script>
